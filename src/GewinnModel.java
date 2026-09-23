39ee0ece5d6f669245274ba41e7d776a972fa3cf
Sonja Stix <sstix@student.tgm.ac.at>
2026-09-23 08:44:49 +0200

    Konstruktor und getter-Methoden hinzugefügt

--- src/GewinnModel.java
+++ src/GewinnModel.java
@@ -3,4 +3,23 @@ public class GewinnModel {
     private int spielerZahl;
     private int computerZahl;
     private int rundenErgebnis;
+
+    public GewinnModel() {
+        this.gesamtPunkte = 30;
+        this.spielerZahl = 0;
+        this.computerZahl = 0;
+        this.rundenErgebnis = 0;
+    }
+
+    public int getGesamtPunkte() {
+        return gesamtPunkte;
+    }
+
+    public int getComputerZahl() {
+        return computerZahl;
+    }
+
+    public int getRundenErgebnis() {
+        return rundenErgebnis;
+    }
 }
