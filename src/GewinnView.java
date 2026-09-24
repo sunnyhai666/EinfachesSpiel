a00aa6ed23542a9e9be71a8cf09c74037f06ccc7
Sonja Stix <sstix@student.tgm.ac.at>
2026-09-24 13:49:14 +0200

    die Attribute der Klasse GewinnView geschrieben

--- src/GewinnView.java
+++ src/GewinnView.java
@@ -1,5 +1,14 @@
 import javax.swing.*;
 
 public class GewinnView extends JPanel {
+    private JLabel rundenErgebnis= new JLabel("Rundenergebnis");
+    private JLabel gesamtPunkte= new JLabel("Gesamtpunkte");
+    private JLabel deineZahl= new JLabel("Deine Zahl:");
+    private JLabel computerZahl=  new JLabel("Computer:");
+    private JButton nochmal= new JButton("Noch einmal!");
 
+    private JTextField computerOutput= new JTextField();
+    private JTextField input;
+    private JLabel runde;
+    private JLabel punkte;
 }
