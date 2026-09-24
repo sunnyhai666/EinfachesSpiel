f1784acaf688901c8f66a76493a4ce4a14985194
Sonja Stix <sstix@student.tgm.ac.at>
2026-09-24 13:48:39 +0200

    Klasse GewinnFrame fertig

--- src/GewinnFrame.java
+++ src/GewinnFrame.java
@@ -1,5 +1,14 @@
 import javax.swing.*;
 
 public class GewinnFrame extends JFrame{
+    public GewinnFrame() {
+        setTitle("Zahlen-Gewinnspiel (v1.0");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setSize(500,500);
+        setLocationRelativeTo(null);
 
+        setContentPane(new GewinnView());
+
+        setVisible(true);
+    }
 }
