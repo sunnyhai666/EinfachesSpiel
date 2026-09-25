dc9414087eda198560c3187ddd49d4c4c97253d1
Sonja Stix <sstix@student.tgm.ac.at>
2026-09-25 10:37:42 +0200

    GewinnView Gewonnen oder Verloren Abfrage hinzugefügt, Try-Catch falls eingegebene Zahl nicht zwischen 1-9 ist und falls ein Buchstabe eingegeben wird

--- src/GewinnFrame.java
+++ src/GewinnFrame.java
@@ -2,7 +2,7 @@ import javax.swing.*;
 
 public class GewinnFrame extends JFrame{
     public GewinnFrame() {
-        setTitle("Zahlen-Gewinnspiel (v1.0");
+        setTitle("Zahlen-Gewinnspiel (v1.0)");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(500,500);
         setLocationRelativeTo(null);
--- src/GewinnView.java
+++ src/GewinnView.java
@@ -18,19 +18,74 @@ public class GewinnView extends JPanel {
     private JLabel punkte = new JLabel();
 
     public GewinnView() {
-        this.setLayout(new GridLayout(4, 2));
+        this.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+
+        //Button
+        c.gridy = 4;
+        c.gridwidth = 2;
+        add(nochmal, c);
+        c.gridwidth = 1;
+
+
+        //Spalte 1
+        c.fill = GridBagConstraints.BOTH;
+        c.gridx = 0;
+        c.weightx = 1.0;
+
+        c.gridy = 0;
+        c.weighty = 0.2;
+        add(rundenErgebnis, c);
+
+        c.gridy = 1;
+        c.weighty = 0.2;
+        add(runde, c);
+
+        c.gridy = 2;
+        c.weighty = 0.2;
+        add(deineZahl, c);
+
+        c.gridy = 3;
+        c.weighty = 2.0;
+        add(input, c);
+
+
+        //Spalte 2
+        c.gridx = 1;
+        c.weightx = 1.0;
+
+        c.gridy = 0;
+        c.weighty = 0.2;
+        add(gesamtPunkte, c);
+
+        c.gridy = 1;
+        c.weighty = 0.2;
+        add(punkte, c);
+
+        c.gridy = 2;
+        c.weighty = 0.2;
+        add(computerZahl, c);
+
+        c.gridy = 3;
+        c.weighty = 2.0;
+        add(computerOutput, c);
+
+
         computerOutput.setEnabled(false);
+        runde.setBackground(Color.WHITE);
+        punkte.setBackground(Color.WHITE);
 
-        add(rundenErgebnis);
-        add(gesamtPunkte);
-        add(runde);
-        add(punkte);
-        add(deineZahl);
-        add(computerZahl);
-        add(input);
-        add(computerOutput);
-        //add(nochmal);
+        runde.setOpaque(true);
+        runde.setText("Tippe eine Zahl von 0 bis 9");
 
+        punkte.setOpaque(true);
+        punkte.setText("Gesamtpunkte: 30");
+
+        input.setHorizontalAlignment(JTextField.CENTER);
+        computerOutput.setHorizontalAlignment(JTextField.CENTER);
+
+        input.setFont(new Font("", Font.PLAIN, 50));
+        computerOutput.setFont(new Font("", Font.PLAIN, 50));
 
         JButtonHandler bh = new JButtonHandler();
         nochmal.addActionListener(bh);
@@ -43,6 +98,8 @@ public class GewinnView extends JPanel {
             Object o  = e.getSource();
 
             if (o == nochmal) {
+                runde.setForeground(Color.BLACK);
+                input.setEnabled(true);
                 runde.setText("Tippe eine Zahl von 1 bis 9");
                 punkte.setText("Gesamtpunkte: 30");
                 input.setText("");
@@ -50,13 +107,39 @@ public class GewinnView extends JPanel {
             }
 
             if (o == input) {
-                model.berechneComputerZahl();
-                computerOutput.setText(Integer.toString(model.getComputerZahl()));
+                try {
+                    int zahl = Integer.parseInt(input.getText());
 
-                model.berechneRunde(Integer.parseInt(input.getText()));
-                runde.setText(Integer.toString(model.getRundenErgebnis()));
+                    if (zahl >= 1 && zahl <= 9) {
+                        model.berechneComputerZahl();
+                        computerOutput.setText(Integer.toString(model.getComputerZahl()));
 
-                punkte.setText(Integer.toString(model.getGesamtPunkte()));
+                        model.berechneRunde(zahl);
+                        runde.setText(Integer.toString(model.getRundenErgebnis()));
+
+                        punkte.setText(Integer.toString(model.getGesamtPunkte()));
+
+                        if (model.hatGewonnen()) {
+                            input.setEnabled(false);
+                            runde.setForeground(Color.GREEN);
+                            runde.setText("Gewonnen :D");
+                        }
+
+                        if (model.hatVerloren()) {
+                            input.setEnabled(false);
+                            runde.setForeground(Color.RED);
+                            runde.setText("Verloren D:");
+                        }
+
+                    } else {
+                        runde.setForeground(Color.RED);
+                        runde.setText("Bitte eine Zahl von 1 bis 9 eingeben!");
+                    }
+
+                } catch (NumberFormatException ex) {
+                    runde.setForeground(Color.RED);
+                    runde.setText("Bitte eine Zahl von 1 bis 9 eingeben!");
+                }
             }
         }
     }
